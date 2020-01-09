e1021a8cc5cbad89f737ac29cce61b2ed35fe256
Francois Hamel <francois.hamel@gmail.com>
2020-01-09 11:42:19 -0500

    adding CMake inteface only lib for DirectXMath

--- src/mdx/platform.h
+++ src/mdx/platform.h
@@ -1,11 +1,8 @@
 #pragma once
 
-//warning C4820 : 'xxxxx' : 'n' bytes padding added after data member 'yyyyy'
-#pragma warning(disable: 4820)
-//warning C5039 : 'xxxxxx' : pointer or reference to potentially throwing function passed to 'extern "C"' function under - EHc.Undefined behavior may occur if this function throws an exception.
-#pragma warning(disable: 5039)
-//warning C5045: Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified
-#pragma warning(disable: 5045)
+#pragma warning(disable: 4820) //warning C4820 : 'xxxxx' : 'n' bytes padding added after data member 'yyyyy'
+#pragma warning(disable: 5039) //warning C5039 : 'xxxxxx' : pointer or reference to potentially throwing function passed to 'extern "C"' function under - EHc.Undefined behavior may occur if this function throws an exception.
+#pragma warning(disable: 5045) //warning C5045: Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified
 
 #include <sdkddkver.h>
 #undef _WIN32_WINNT
@@ -30,17 +27,20 @@
 
 //D3DX12 helpers
 #pragma warning(push)
-//warning C4061: enumerator 'xxxxx' in switch of enum 'yyyy' is not explicitly handled by a case label
-#pragma warning(disable: 4061)
-//warning C4365: '=': conversion from 'xxxxx' to 'yyyy', signed/unsigned mismatch
-#pragma warning(disable: 4365)
+#pragma warning(disable: 4061) //warning C4061: enumerator 'xxxxx' in switch of enum 'yyyy' is not explicitly handled by a case label
+#pragma warning(disable: 4365) //warning C4365: '=': conversion from 'xxxxx' to 'yyyy', signed/unsigned mismatch
 #define D3DX12_NO_STATE_OBJECT_HELPERS
 #include <d3dx12/d3dx12.h>
 #pragma warning(pop)
 
 //for PIX support
 #pragma warning(push)
-//warning C4365 : 'return' : conversion from 'LONGLONG' to 'UINT64', signed / unsigned mismatch
-#pragma warning(disable: 4365)
+#pragma warning(disable: 4365) //warning C4365 : 'return' : conversion from 'xxxxx' to 'yyyyy', signed / unsigned mismatch
 #include <WinPixEventRuntime/pix3.h>
+#pragma warning(pop)
+
+//DirectXMath
+#pragma warning(push)
+#pragma warning(disable: 4668) //warning C4668: 'xxxxx' is not defined as a preprocessor macro, replacing with '0' for '#if/#elif'
+#include <DirectXMath.h>
 #pragma warning(pop)
